1cf93032f9a5963b2109a53e3bf009f926c93cc7
SQ <1653216274@qq.com>
2024-08-30 14:54:50 +0800

    优化图形打印

--- test/chap1/图形打印.py
+++ test/chap1/图形打印.py
@@ -19,13 +19,17 @@ for i in range(0,5):
 
 print('-'*20)
 
-ROW=3
+ROW=5
+'''
 for i in range(0,ROW):
     for j in range(i,ROW-1):
         print('  ',end='')
     for j in range(0,1+2*i):
         print('* ', end='')
     print()
+'''#优化如下
+for i in range(0,ROW):
+    print('  '*(ROW-i-1)+'* '*(1+2*i))
 
 print('-'*20)
 
@@ -34,6 +38,7 @@ while not row%2:
     row=eval(input('请输入一个奇数：'))
 on=(row+1)//2
 low=(row-1)//2
+'''
 for i in range(0,on):
     for j in range(i,on-1):
         print('  ',end='')
@@ -46,9 +51,15 @@ for i in range(0,low):
     for j in range(0,2*low-1-2*i):
         print('* ',end='')
     print()
+'''#优化如下
+for i in range(0,on):
+    print('  '*(on-i-1)+'* '*(1+2*i))
+for i in range(0,low):
+    print('  '*(1+i)+'* '*(2*low-2*i-1))
 
 print('-'*20)
 
+'''
 for i in range(0,on):
     for j in range(i,on-1):
         print('  ',end='')
@@ -66,4 +77,17 @@ for i in range(0,low):
             print('* ',end='')
         else:
             print('  ',end='')
-    print()
+'''
+for i in range(0,on):
+    print('  '*(on-i-1)+'* ',end='')
+    if i==0:
+        print()
+    else:
+        print('  '*(2*i-1)+'* ')
+for i in range(0,low):
+    print('  '*(1+i)+'* ',end='')
+    if i != low-1:
+        print('  '*(2*low-2*i-3)+'* ')
+    else:
+        pass
+
