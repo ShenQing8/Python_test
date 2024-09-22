b72fa52b2c08a33f1aa93f4b33a8ba6f6a3a20e9
SQ <1653216274@qq.com>
2024-09-22 20:50:16 +0800

    correct

--- test/chap1/test1.py
+++ test/chap1/test1.py
@@ -19,5 +19,7 @@ print(f'{nums[0:2]+nums[3:5]},{type(nums[0:2]+nums[3:5])}')
 k = [x*y for x in range(1,5) if x > 2 for y in range(1,4) if y < 3]
 print(k)
 
-# s, p = eval(input())
-# print(s, p)
+s, p = (input()).split(' ')
+s = int(s)
+p = int(p)
+print(s, p,type(s), type(p))
